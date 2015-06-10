7a89ce90b488333fe6097a5fad136ef434dab8e6
ankita <akkitawar491@gmail.com>
2015-06-10 15:20:26 -0700

    2 files added

---
+++ parameter.py
@@ -0,0 +1,14 @@
+def move(time,speed):
+	 print("I am moving on" + str(speed )+ kmph" + " + "at" + str(time))
+     x = time+1
+     y = speed-1
+     print("I am moving on" + str(x)+ "kmph" + "at" + str(y))
+     speed=speed
+    
+
+move(10,5)
+print("__________________")
+move(2,10)
+print("__________________")
+move(3,11)
+print("__________________")
---
+++ star.py
@@ -0,0 +1,16 @@
+
+ 
+
+
+
+  def box(width,height):
+ 	print( width * "*")
+ 	for i in range(height -  2):
+ 	    print ("*" + (width - 2) * " " + "*")
+    print (width * "*")
+
+    print (12 * "*")
+    print  (5  * "*")
+    print (10 * "*")
+ box(10,3)
+ box(4,7)
